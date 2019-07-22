7896fe0364c1c006c95b04976bfeaf612bfb6a00
monjar <amirali.mnj@gmail.com>
2019-07-22 16:13:45 +0430

    query annot added

--- src/main/java/ind/amirali/Repository/PersonRepository.java
+++ src/main/java/ind/amirali/Repository/PersonRepository.java
@@ -2,6 +2,7 @@ package ind.amirali.Repository;
 
 import ind.amirali.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
@@ -9,5 +10,8 @@ public interface PersonRepository extends MongoRepository<User, String> {
     public List<User> findByBalanceGreaterThan(Double balance);
 
     public List<User> findByBalanceIsGreaterThan(Double balance);
+
+    @Query("{lastName : {$regex : ?0}}")
+    public List<User> finfByRegex(String lastNameRegEx);
 
 }
