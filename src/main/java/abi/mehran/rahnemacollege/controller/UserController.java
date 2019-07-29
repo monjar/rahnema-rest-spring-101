fdbe221192b0d22b8f2f1b8820ecd506644d753c
Seyed Mehran Abghari <mehran.ab80@gmail.com>
2019-07-29 13:03:57 +0430

    restful assembler

--- src/main/java/abi/mehran/rahnemacollege/controller/UserController.java
+++ src/main/java/abi/mehran/rahnemacollege/controller/UserController.java
@@ -3,36 +3,49 @@ package abi.mehran.rahnemacollege.controller;
 import abi.mehran.rahnemacollege.domain.UserInput;
 import abi.mehran.rahnemacollege.model.User;
 import abi.mehran.rahnemacollege.service.UserServiceImpl;
+import abi.mehran.rahnemacollege.util.assembler.UserResourceAssembler;
 import abi.mehran.rahnemacollege.util.exception.EntityNotFoundException;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 public class UserController {
 
     private final UserServiceImpl service;
+    private final UserResourceAssembler assembler;
 
-    public UserController(UserServiceImpl service) {
+    public UserController(UserServiceImpl service, UserResourceAssembler assembler) {
         this.service = service;
+        this.assembler = assembler;
     }
 
     @GetMapping("/users")
-    public List<User> all() {
-        return service.all();
+    public Resources<Resource<User>> all() {
+        List<Resource<User>> users = service.all().stream()
+                .map(this.assembler::assemble)
+                .collect(Collectors.toList());
+        return new Resources<>(users, linkTo(methodOn(UserController.class).all()).withSelfRel());
     }
 
     @PostMapping("/users")
-    public User add(@RequestBody UserInput userInput) {
+    public Resource<User> add(@RequestBody UserInput userInput) {
         User user = userInput.toUser();
-        return service.add(user);
+        return this.assembler.assemble(user);
     }
 
     @GetMapping("/user/{id}")
-    public User one(@PathVariable String id) {
+    public Resource<User> one(@PathVariable String id) {
         Optional<User> optionalUser = service.get(id);
-        return optionalUser.orElseThrow(() -> new EntityNotFoundException(id));
+        User user = optionalUser.orElseThrow(() -> new EntityNotFoundException(id));
+        return this.assembler.assemble(user);
     }
 
     @DeleteMapping("/user/{id}")
--- src/main/java/abi/mehran/rahnemacollege/util/assembler/UserResourceAssembler.java
+++ src/main/java/abi/mehran/rahnemacollege/util/assembler/UserResourceAssembler.java
@@ -1,13 +1,20 @@
 package abi.mehran.rahnemacollege.util.assembler;
 
+import abi.mehran.rahnemacollege.controller.UserController;
 import abi.mehran.rahnemacollege.model.User;
 import org.springframework.hateoas.Resource;
 import org.springframework.stereotype.Component;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
 @Component
 public class UserResourceAssembler {
 
-    public static Resource<User> toResourse(User user) {
-        return null;
+    public Resource<User> assemble(User user) {
+        return new Resource<>(user,
+                linkTo(methodOn(UserController.class).one(user.getId())).withSelfRel(),
+                linkTo(methodOn(UserController.class).all()).withRel("all")
+        );
     }
 }
